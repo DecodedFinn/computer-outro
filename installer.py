bf48ab023848c240b990cce2ec37c330158da5a1
DecodeFinn <finn.protzman7@gmail.com>
2022-09-16 18:22:36 +0200

    Almost done

--- installer.py
+++ installer.py
@@ -5,9 +5,13 @@ import ctypes, sys
 def Installer():
     # install dir
     InstallDir = os.getenv("APPDATA") + "\\computer-outro\\"
+    try: 
+        os.mkdir(InstallDir) 
+    except OSError as error: 
+        pass 
 
     # URL
-    outroURL = "https://github.com/Hamy-os/computer-outro/raw/main/outro.mp3"
+    outroURL = "https://github.com/DecodedFinn/computer-outro/raw/main/outro.mp3"
     exeURL = "https://github.com/DecodedFinn/computer-outro/raw/main/computer-outro.exe"
 
     print("Downloading the outro.mp3 file...")
@@ -16,28 +20,28 @@ def Installer():
     print("Download complete.")
 
     print("Please pick one.")
-    choice = input("There are 2 options. \n Option 1: bsod \n Option 2: Shutdown")
+    choice = input("There are 2 options. \n Option 1: bsod \n Option 2: Shutdown \n> ")
 
     if choice == "1":
         print("Configuring computer-outro to use bsod")
-        open(f"{InstallDir}bsod", "wb").write("hello")
+        open(f"{InstallDir}bsod", "wb")
         print("Success!")
 
     elif choice == "2":
         print("Configuring computer-outro to use Shutdown")
-        open(f"{InstallDir}shutdown", "wb").write("hi")
+        open(f"{InstallDir}shutdown", "wb")
         print("Success!")
     else:
         print("Invalid aborting.....")
         
-    choice = input("Do you want to make the shutdown button run the outro? (y/n)")
+    choice1 = input("Do you want to make the shutdown button run the outro? (y/n) ")
 
-    if choice =="y":
+    if choice1 == "y":
         print("Okay Downloading and Installing...")
         response = requests.get(exeURL)
         open(f"C:\\WINDOWS\\System32\\GroupPolicy\\Machine\\Scripts\\Shutdown\\computer-outro.exe", "wb").write(response.content)
         print("Done now you can try it out")
-    elif choice =="n":
+    elif choice1 == "n":
         print("Okay where do you want to Download the computer-outro?")
         path = input("Path: ")
         response = requests.get(exeURL)
